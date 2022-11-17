a70135e4ba490c391f40d632af746caf39df88d5
kkm0406 <sdjmc30412@naver.com>
2022-11-17 21:00:53 +0900

    <Feat> :  Database ---------------------------------------- 1. api 코드 백엔드 처리 2. mysql 실행 함수 생성 ---------------------------------------- 1. view 쿼리 2. normalization

--- main.py
+++ main.py
@@ -1,7 +1,42 @@
 from fastapi import FastAPI, File, UploadFile
 import os
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+import db
+# import pymysql
 
+#회원가입 정보
+class SignUp(BaseModel):
+    name: str
+    id: str
+    pw: str
+    type: int #1이면 학생, 2이면 선생
+
+#아이디 중복 확인
+class ID(BaseModel):
+    id:str
+
+#로그인
+class Login(BaseModel):
+    id: str
+    pw: str
+    
 app = FastAPI()
+
+#CORS(https://www.jasonchoi.dev/posts/fastapi/cors-allow-setting)
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+# 미들웨어 추가 -> CORS 해결위해 필요(https://ghost4551.tistory.com/46)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
@@ -30,3 +65,15 @@ async def root(num:int):
     temp=os.popen('isolate --cg -b '+str(num)+' --init').read()
     return {"message": temp}
 
+
+@app.post('/login/')
+async def login(user:Login):
+    return {'code': db.check_db(user)}
+
+@app.post("/signup/")
+async def create_user(user: SignUp):
+    return {"code":db.insert_db(user)}
+
+@app.post("/checkids/")
+async def check_ids(id: ID):
+    return {"code": db.find_ids(id.id)} 
