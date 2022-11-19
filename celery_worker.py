b9ad907aa793ace15cf64989f055d515484ecb2e
styughjvbn <3338761@naver.com>
2022-11-20 02:39:13 +0900

    <Feat> : 제출 요청 api 구체화 ---------------------------------------------------------- 1. post 제출 요청 구현 2. 요청받은 제출의 코드를 병렬실행하는 task 구현 3. isolate id별 폴더 생성 ---------------------------------------------------------- 1. 데이터베이스 연결해서 문제id로 testcase, 제한사항 가져오기 2. 실행 결과 데이터베이스에 업데이트하기 3. callback url 활용 생각하

--- celery_worker.py
+++ celery_worker.py
@@ -1,10 +1,10 @@
 from celery_app import celery_task
 import time
 import redis
+import subprocess
 
 @celery_task.task(ignore_result=True)
-def divide(x, y):
-
+def process_sub(taskid,stdid,subtime,sourcecode,callbackurl,token ):
     i=1
     with redis.StrictRedis(host='127.0.0.1', port=6379, db=0) as conn:
         while i<9:
@@ -13,7 +13,24 @@ def divide(x, y):
                 conn.set(str(i),1)
                 break
             i+=1
-        #isolate가 실행될 곳
-        time.sleep(30)
+        #todo
+        # 데이터베이스 연결해서 문제id로 testcase, 제한사항 가져오기
+        # 실행 결과 데이터베이스에 업데이트하기
+        # callback url 활용 생각하기
+        
+        #isolate id별로 초기화
+        init_result=subprocess.run(['isolate', '--cg', '-b',str(i),'--init'],capture_output=True)
+        
+        # 실행 파일 생성
+        f1=open('/var/local/lib/isolate/'+str(i)+'/box/src.py','w')
+        f1.write(sourcecode)
+        f1.close()
+
+        #isolate 환경에서 실행
+        subprocess.run('isolate --meta /home/sjw/COCO_Back_End/box/'+str(i)+'/meta/meta.txt --cg -t 2 -b '+str(i)+' --run /usr/bin/python3 src.py < /home/sjw/COCO_Back_End/box/'+str(i)+'/in/in.txt > /home/sjw/COCO_Back_End/box/'+str(i)+'/out/out.txt',shell=True)
+        
+        #isolate id 삭제
+        init_result=subprocess.run(['isolate', '--cg', '-b',str(i),'--cleanup'],capture_output=True)
+
+        time.sleep(5)
         conn.set(str(i),"0")
-    return x / y
