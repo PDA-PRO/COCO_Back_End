5b17e43c154da31c43b6efe391fdeaffad6ba5fe
styughjvbn <3338761@naver.com>
2022-11-20 02:40:36 +0900

    <Feat> : 제출 요청 api 구체화 ---------------------------------------------------------- 1. post 제출 요청 구현 2. 요청받은 제출의 코드를 병렬실행하는 task 구현 3. isolate id별 폴더 생성 ---------------------------------------------------------- 1. 데이터베이스 연결해서 문제id로 testcase, 제한사항 가져오기 2. 실행 결과 데이터베이스에 업데이트하기 3. callback url 활용 생각

--- main.py
+++ main.py
@@ -1,41 +1,24 @@
-from fastapi import FastAPI, File, UploadFile
-from celery_worker import divide
-import os
+from fastapi import FastAPI, File, UploadFile,Path
+from celery_worker import process_sub
+import subprocess
 import redis
+from typing import Union
+from pydantic import BaseModel
 app = FastAPI()
 
+class Submission(BaseModel):
+    taskid: int
+    stdid: int
+    time: int
+    sourcecode:str
+    callbackurl:str
+    token:str
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.post("/submission_test")
-async def root(code:UploadFile,box_id:int,stdin:str):
-    f=open("/var/local/lib/isolate/"+str(box_id)+"/box/2temp.py",'wb+')
-    content = await code.read()
-    f.write(content)
-    f.close()
-    f1=open('stdin.txt','w')
-    f1.write(stdin)
-    f1.close()
-    f1=open('stdout.txt','w')
-    f1.close()
-    temp=os.popen('isolate --cg -b '+str(box_id)+' -M ./metadata.txt -d /etc:noexec --run -- /usr/bin/python3 2temp.py < ./stdin.txt > ./stdout.txt 2> ./stderr.txt').read()
-    f1=open('stdout.txt','r')
-    result=f1.readline()
-    f1.close()
-    return {"message": result}
-
-@app.post("/init")
-async def root(num:int):
-    temp=os.popen('isolate --cg -b '+str(num)+' --init').read()
-    return {"message": temp}
-
-@app.get("/work")
-async def work(task_id: str, input_a: int, input_b: int):
-    divide.apply_async([input_a, input_b], task_id=task_id)
-
-    return {"message": "celery start"}
+@app.post("/submission")
+async def root(sub:Submission):
+    # db에 새로운 제출 저장하기
+    process_sub.apply_async([sub.taskid,sub.stdid,sub.time,sub.sourcecode,sub.callbackurl,sub.token])
+    return {"message": sub}
     
 @app.get("/running_stat")
 async def work_result():
