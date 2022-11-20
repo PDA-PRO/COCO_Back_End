367cca98070a84b13dffc55d5f7b15a40bf971d6
styughjvbn <3338761@naver.com>
2022-11-20 21:23:43 +0900

    <Refactor> : 디렉토리 리팩토링 ---------------------------------------------------------- 1. 디렉토리 리팩토링 ----------------------------------------------------------

--- app/main.py
+++ app/main.py
@@ -1,12 +1,32 @@
 from fastapi import FastAPI
-from api.routers import submission
+from api.routers import submission,login
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+#CORS(https://www.jasonchoi.dev/posts/fastapi/cors-allow-setting)
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+# 미들웨어 추가 -> CORS 해결위해 필요(https://ghost4551.tistory.com/46)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 #라우터 설정
 app.include_router(submission.router)
+app.include_router(login.router)
 
 @app.get("/test")
 async def hello_test():
     return {"message": 'hello'}
     
+
+
+
