6ccfedbc8b9797e2f295889d1e3f76f7bf936826
styughjvbn <3338761@naver.com>
2022-11-24 17:07:49 +0900

    <Feat> :  db model 작성 ---------------------------------------------------------- 1. db 테이블에 맞는 model 작성 ----------------------------------------------------------

---
+++ app/models/submission.py
@@ -0,0 +1,16 @@
+class Submission():
+    task_id:int
+    user_id:str
+    sub_id:int
+    status_id:int
+    source_code:str
+    stdout:str
+    time:str
+    stderr:str
+    token:str
+    callback_url:str
+    exit_code:int
+    message:str
+    exit_signal:str
+    compile_output:str
+    number_of_runs:int
