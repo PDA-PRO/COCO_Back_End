30af803817642f1adb3702ed0fa153dc0c4d0c89
kkm0406 <sdjmc30412@naver.com>
2022-12-05 19:37:05 +0900

    <Feat> :  Board ---------------------------------------- 1. 문제 상세 페이지 api 구현 2. 빠른 글쓰기 api 구현
    ----------------------------------------

--- app/api/routers/board.py
+++ app/api/routers/board.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from crud.board import CrudBoard
 
 router = APIRouter()
+crudBoard = CrudBoard()
 
 class Board(BaseModel):
     id: int
@@ -14,6 +15,20 @@ class Board(BaseModel):
     views: int
     comments: int
 
+class FastWrite(BaseModel):
+    user_id: str
+    title: str
+    context: str
+
+
 @router.get('/board/', tags = ['board'])
 async def check_board():
-    return CrudBoard.check_board()
+    return crudBoard.check_board()
+
+@router.get('/board/{board_id}', tags = ['board'] )
+async def detail_board(board_id: int):
+    return crudBoard.board_detail(board_id)
+
+@router.post('/fastWrite/', tags = ['board'])
+async def fast_write(fastWrite: FastWrite):
+    return {'code': crudBoard.fast_write(fastWrite)}
