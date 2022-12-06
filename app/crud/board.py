e7e000a2e14e4c0c7db04f8c3e8722180ddc6a79
kkm0406 <sdjmc30412@naver.com>
2022-12-07 02:36:50 +0900

    <Feat> :  게시판 ---------------------------------------- 1. 게시글 작성자 리턴 추가 2. 게시판 시간순으로 정렬 3. 게시판 조회수 구현
    ----------------------------------------
    1. 내가 작성한 게시글 삭제
    2. 내가 작성한 댓글 삭제

--- app/crud/board.py
+++ app/crud/board.py
@@ -6,7 +6,7 @@ db_server = db.db_server
 
 class CrudBoard():
     def check_board(self):
-        sql = f'select * from view_board'
+        sql = f'select * from view_board order by time desc;'
         result = self.execute_mysql(sql)
         contents = []
         for i in result:
@@ -24,8 +24,15 @@ class CrudBoard():
         return contents
 
     def board_detail(self, board_id):
-        print(board_id)
-        sql = f"SELECT * FROM coco.boards where id = '{board_id}';"
+        views_sql = f"SELECT views FROM coco.boards WHERE id = '{board_id}';"
+        cnt_views = self.execute_mysql(views_sql)
+        update_views = f"UPDATE `coco`.`boards` SET `views` = '{cnt_views[0][0]+1}' WHERE (`id` = '{board_id}');"
+        self.insert_mysql(update_views)
+        sql = f"""
+            SELECT b.id, b.context, b.title, b.rel_task, b.time, b.category, b.likes, b.views, b.comments, i.user_id
+            FROM coco.boards AS b, coco.boards_ids AS i
+            WHERE b.id = i.board_id AND b.id = '{board_id}';
+        """
         result = self.execute_mysql(sql)
         comments_sql = f"""
             SELECT c.id, c.context, c.write_time, c.likes, i.user_id, i.board_id
@@ -44,11 +51,12 @@ class CrudBoard():
             'likes': result[0][6],
             'views': result[0][7],
             'comments': result[0][8],
+            'user_id': result[0][9],
             'comments_datail': comments_result
         }
 
     def fast_write(self, fastWrite):
-        sql = f"INSERT INTO `coco`.`boards` (`context`, `title`, `time`, `category`, `likes`, `views`, `comments`) VALUES ('{fastWrite.context}', '{fastWrite.title}', '{datetime.now().date()}', '3', '0', '0', '0');"
+        sql = f"INSERT INTO `coco`.`boards` (`context`, `title`, `time`, `category`, `likes`, `views`, `comments`) VALUES ('{fastWrite.context}', '{fastWrite.title}', '{datetime.now()}', '3', '0', '0', '0');"
         self.insert_mysql(sql)
         user_sql = f"SELECT * FROM coco.boards order by id;"
         result = self.execute_mysql(user_sql)
@@ -68,12 +76,18 @@ class CrudBoard():
         return 1
 
     def write_comment(self, commentInfo):
-        print(commentInfo)
-        sql = f"INSERT INTO `coco`.`comments` (`context`, `write_time`, `likes`) VALUES ('{commentInfo.context}', '{datetime.now().date()}', '0');"
+        sql = f"INSERT INTO `coco`.`comments` (`context`, `write_time`, `likes`) VALUES ('{commentInfo.context}', '{datetime.now()}', '0');"
         idx_sql  = f"select LAST_INSERT_ID();"
         last_idx = self.insert_comments(sql, idx_sql)
         comment_sql = f"INSERT INTO `coco`.`comments_ids` (`comment_id`, `user_id`, `board_id`) VALUES ('{last_idx}', '{commentInfo.user_id}', '{commentInfo.board_id}');"
         self.insert_mysql(comment_sql)
+        cnt_sql = f"""
+            select count(*) from coco.comments_ids 
+            where board_id = '{commentInfo.board_id}';
+        """
+        cnt = self.execute_mysql(cnt_sql)
+        update_sql = f"UPDATE `coco`.`boards` SET `comments` = '{cnt[0][0]}' WHERE (`id` = '{commentInfo.board_id}');"
+        self.insert_mysql(update_sql)
         return 1
 
     def comment_likes(self, commentLikes):
