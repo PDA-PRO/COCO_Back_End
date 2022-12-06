5529b9997cdf14451b3accb462052bd4199a6617
kkm0406 <sdjmc30412@naver.com>
2022-12-07 01:27:26 +0900

    <Feat> :  게시판 ---------------------------------------- 1. 게시판 좋아요 api 구현 2. 댓글 작성 api 구현 3. 댓글 좋아요 api 구현
    ----------------------------------------
    1. 내가 작성한 게시글 삭제
    2. 내가 작성한 댓글 삭제

--- app/api/routers/board.py
+++ app/api/routers/board.py
@@ -20,6 +20,27 @@ class FastWrite(BaseModel):
     title: str
     context: str
 
+class BoardLikes(BaseModel):
+    user_id: str
+    board_id: int
+    likes: int
+    type: bool
+
+class CommentLikes(BaseModel):
+    user_id: str
+    board_id: int
+    comment_id: int
+    likes: int
+    type: bool
+
+class CommentInfo(BaseModel):
+    user_id: str
+    context: str
+    board_id: int
+
+
+
+
 
 @router.get('/board/', tags = ['board'])
 async def check_board():
@@ -28,6 +49,18 @@ async def check_board():
 @router.get('/board/{board_id}', tags = ['board'] )
 async def detail_board(board_id: int):
     return crudBoard.board_detail(board_id)
+
+@router.post('/board_likes/', tags = ['board'])
+async def board_likes(boardLikes: BoardLikes):
+    return {'code': crudBoard.board_likes(boardLikes)}
+
+@router.post('/comment/', tags = ['board'])
+async def write_comment(commentInfo: CommentInfo):
+    return {'code': crudBoard.write_comment(commentInfo)}
+
+@router.post('/comment_likes/', tags = ['board'])
+async def comment_likes(commentLikes: CommentLikes):
+    return {'code': crudBoard.comment_likes(commentLikes)}
 
 @router.post('/fastWrite/', tags = ['board'])
 async def fast_write(fastWrite: FastWrite):
--- app/crud/board.py
+++ app/crud/board.py
@@ -27,6 +27,13 @@ class CrudBoard():
         print(board_id)
         sql = f"SELECT * FROM coco.boards where id = '{board_id}';"
         result = self.execute_mysql(sql)
+        comments_sql = f"""
+            SELECT c.id, c.context, c.write_time, c.likes, i.user_id, i.board_id
+            FROM coco.comments AS c, comments_ids AS i
+            WHERE i.comment_id = c.id AND i.board_id = '{board_id}';
+        """
+        comments_result = self.execute_mysql(comments_sql)
+
         return {
             'id': result[0][0],
             'context': result[0][1],
@@ -36,7 +43,8 @@ class CrudBoard():
             'category': result[0][5],
             'likes': result[0][6],
             'views': result[0][7],
-            'comments': result[0][8]
+            'comments': result[0][8],
+            'comments_datail': comments_result
         }
 
     def fast_write(self, fastWrite):
@@ -47,6 +55,47 @@ class CrudBoard():
         board_sql = f"INSERT INTO `coco`.`boards_ids` (`board_id`, `user_id`) VALUES ('{result[-1][0]}', '{fastWrite.user_id}');"
         self.insert_mysql(board_sql)
         return 1
+
+    def board_likes(self, boardLikes):
+        update_sql = f"UPDATE `coco`.`boards` SET `likes` = '{boardLikes.likes}' WHERE (`id` = '{boardLikes.board_id}');"
+        self.insert_mysql(update_sql)
+        type_sql = ""
+        if boardLikes.type:
+            type_sql = f"DELETE FROM `coco`.`boards_likes` WHERE (`user_id` = '{boardLikes.user_id}') and (`boards_id` = '{boardLikes.board_id}');"
+        else:
+            type_sql = f"INSERT INTO `coco`.`boards_likes` (`user_id`, `boards_id`) VALUES ('{boardLikes.user_id}', '{boardLikes.board_id}');" 
+        self.insert_mysql(type_sql)
+        return 1
+
+    def write_comment(self, commentInfo):
+        print(commentInfo)
+        sql = f"INSERT INTO `coco`.`comments` (`context`, `write_time`, `likes`) VALUES ('{commentInfo.context}', '{datetime.now().date()}', '0');"
+        idx_sql  = f"select LAST_INSERT_ID();"
+        last_idx = self.insert_comments(sql, idx_sql)
+        comment_sql = f"INSERT INTO `coco`.`comments_ids` (`comment_id`, `user_id`, `board_id`) VALUES ('{last_idx}', '{commentInfo.user_id}', '{commentInfo.board_id}');"
+        self.insert_mysql(comment_sql)
+        return 1
+
+    def comment_likes(self, commentLikes):
+        update_sql = f"UPDATE `coco`.`comments` SET `likes` = '{commentLikes.likes}' WHERE (`id` = '{commentLikes.comment_id}');"
+        self.insert_mysql(update_sql)
+        type_sql = ""
+        if commentLikes.type:
+            type_sql = f"DELETE FROM `coco`.`comments_likes` WHERE (`user_id` = '{commentLikes.user_id}') and (`comment_id` = '{commentLikes.comment_id}');"
+        else:
+            type_sql = f"INSERT INTO `coco`.`comments_likes` (`user_id`, `comment_id`) VALUES ('{commentLikes.user_id}', '{commentLikes.comment_id}');" 
+        self.insert_mysql(type_sql)
+
+    def insert_comments(self, query, idx_query):
+        con = pymysql.connect(host=db_server.host, user=db_server.user, password=db_server.password,
+                            db=db_server.db, charset='utf8')  # 한글처리 (charset = 'utf8')
+        cur = con.cursor()
+        cur.execute(query)
+        cur.execute(idx_query)
+        idx = cur.fetchall()
+        con.commit()
+        con.close()
+        return idx[0][0]
 
     def execute_mysql(self, query):
         con = pymysql.connect(host=db_server.host, user=db_server.user, password=db_server.password,
@@ -64,3 +113,5 @@ class CrudBoard():
         cur.execute(query)
         con.commit()
         con.close()
+    
+
