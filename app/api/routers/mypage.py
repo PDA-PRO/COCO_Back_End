f42e5f80d1af183c965f22b9979f7c81ba9ca2b4
styughjvbn <3338761@naver.com>
2023-01-08 15:21:39 +0900

    <Fix> : mypage api 인증 추가 ------------------------------------------------ 1.   mypage api 인증 추가

--- app/api/routers/mypage.py
+++ app/api/routers/mypage.py
@@ -1,25 +1,26 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from crud.mypage import mypage
 from schemas.mypage import ChangeInfo
+from core import security
 
 router = APIRouter()
 
 @router.get('/myPageOne/{user_id}', tags=['mypage'])
-async def mypage_one(user_id: str):
+async def mypage_one(user_id: str,token: dict = Depends(security.check_token)):
     return mypage.myinfo(user_id)
 
 @router.get('/myPageThree/{user_id}', tags=['mypage'])
-async def mypage_three(user_id: str):
+async def mypage_three(user_id: str,token: dict = Depends(security.check_token)):
     return mypage.myboard(user_id)
 
 @router.post('/delete_myboard{board_id}', tags=['mypage'])
-async def delete_myboard(board_id:int):
+async def delete_myboard(board_id:int,token: dict = Depends(security.check_token)):
     return mypage.delete_myboard(board_id)
 
 @router.post('/changePW/', tags=['mypage'])
-async def change_pw(info: ChangeInfo):
+async def change_pw(info: ChangeInfo,token: dict = Depends(security.check_token)):
     return mypage.change_pw(info)
 
 @router.post('/changeEmail', tags=['mypage'])
-async def change_email(info: ChangeInfo):
+async def change_email(info: ChangeInfo,token: dict = Depends(security.check_token)):
     return mypage.change_email(info)
