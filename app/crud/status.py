1c91f2c0b579981e8d65739a02e40ef9737b5fe5
Jo MINSU <50093223+lookinmin@users.noreply.github.com>
2023-01-20 23:06:57 +0900

    <Feat> : status
    1. 채점상황에 언어 추가

--- app/crud/status.py
+++ app/crud/status.py
@@ -2,12 +2,12 @@ from .base import Crudbase
 
 class CrudStatus(Crudbase):
     def select_status(self):
-        sql="select s.*,t.title, t.diff from coco.status_all as s, coco.task as t where s.task_id=t.id order by s.sub_id desc;"
+        sql="select s.*,t.title, t.diff, t.lan_c, t.lan_py from coco.status_all as s, coco.task as t where s.task_id=t.id order by s.sub_id desc;"
         result = self.select_sql(sql)
         return result
 
     def select_status_user(self,user):
-        sql="select s.*,t.title, t.diff from coco.status_all as s, coco.task as t where s.task_id=t.id and s.user_id=%s order by s.sub_id desc"
+        sql="select s.*,t.title, t.diff, t.lan_c, t.lan_py from coco.status_all as s, coco.task as t where s.task_id=t.id and s.user_id=%s order by s.sub_id desc"
         data=(user)
         result = self.select_sql(sql,data)
         return result
