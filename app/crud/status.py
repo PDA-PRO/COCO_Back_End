129d52a45de8158c92948f15bc73e9d0b3d24afd
kkm0406 <sdjmc30412@naver.com>
2023-01-25 15:27:36 +0900

    <Feat> :  문제 검색 및 채점 상황 ---------------------------------------- 1. 문제 페이지 문제 검색 api 2. 채점 상황 옵션별 정렬 api 3. 채점 상황 문제별 api
    ----------------------------------------

--- app/crud/status.py
+++ app/crud/status.py
@@ -2,14 +2,114 @@ from .base import Crudbase
 
 class CrudStatus(Crudbase):
     def select_status(self):
-        sql="select s.*,t.title from coco.status_all as s, coco.task as t where s.task_id=t.id order by s.sub_id desc;"
+        sql="select * from coco.status_list order by sub_id desc;"
         result = self.select_sql(sql)
         return result
 
-    def select_status_user(self,user):
-        sql="select s.*,t.title from coco.status_all as s, coco.task as t where s.task_id=t.id and s.user_id=%s order by s.sub_id desc"
-        data=(user)
+    def select_status_user(self,user_id, lang, result):
+        if result:
+            if lang == -1:
+                sql = """
+                    select * from coco.status_list 
+                    where user_id = %s and status = 3
+                    order by sub_id desc;
+                """
+                data = (user_id.strip())
+            else:
+                sql = """
+                    select * from coco.status_list 
+                    where user_id = %s and lang = %s and status = 3
+                    order by sub_id desc;
+                """
+                data=(user_id.strip(), lang)
+        else:
+            if lang == -1:
+                sql = """
+                    select * from coco.status_list 
+                    where user_id = %s
+                    order by sub_id desc;
+                """
+                data = (user_id.strip())                
+            else:
+                sql = """
+                    select * from coco.status_list 
+                    where user_id = %s and lang = %s
+                    order by sub_id desc;
+                """
+                data=(user_id.strip(), lang)
         result = self.select_sql(sql,data)
+        return result
+
+    def status_per_task(self, info):
+        #option: 유저별, 언어, 정답
+        if info.option[0]:
+            if info.option[1] == -1:
+                if info.option[2]:
+                    #유저별, 문제 검색, 정답
+                    sql = """
+                        select * from coco.status_list 
+                        where user_id = %s and (title like %s or task_id like %s) and status = 3
+                        order by sub_id desc;
+                    """
+                else:
+                    #유저별, 문제 검색
+                    sql = """
+                        select * from coco.status_list 
+                        where user_id = %s and (title like %s or task_id like %s)
+                        order by sub_id desc;
+                    """
+                data = (info.user_id, '%'+info.task_info+'%', '%'+info.task_info+'%')
+            else:
+                if info.option[2]:
+                    # 유저별, 문제 검색, 언어별, 정답
+                    sql = """
+                        select * from coco.status_list 
+                        where user_id = %s and (title like %s or task_id like %s) and lang = %s and status = 3
+                        order by sub_id desc;
+                    """
+                else:
+                    # 유저별, 문제 검색, 언어별                 
+                    sql = """
+                        select * from coco.status_list 
+                        where user_id = %s and (title like %s or task_id like %s) and lang = %s 
+                        order by sub_id desc;
+                    """
+                data = (info.user_id, '%'+info.task_info+'%', '%'+info.task_info+'%', info.option[1])
+        else:
+            if info.option[1] == -1:
+                if info.option[2]:
+                    #문제 검색, 정답
+                    sql = """
+                        select * from coco.status_list 
+                        where (title like %s or task_id like %s) and status = 3
+                        order by sub_id desc;
+                    """
+                else:
+                    #문제 검색
+                    sql = """
+                        select * from coco.status_list 
+                        where (title like %s or task_id like %s)
+                        order by sub_id desc;
+                    """
+                data = ('%'+info.task_info+'%', '%'+info.task_info+'%')
+            else:
+                if info.option[2]:
+                    # 문제 검색, 언어별, 정답
+                    sql = """
+                        select * from coco.status_list 
+                        where (title like %s or task_id like %s) and lang = %s and status = 3
+                        order by sub_id desc;
+                    """
+                else:
+                    # 문제 검색, 언어별                 
+                    sql = """
+                        select * from coco.status_list 
+                        where (title like %s or task_id like %s) and lang = %s 
+                        order by sub_id desc;
+                    """
+                data = ('%'+info.task_info+'%', '%'+info.task_info+'%', info.option[1])
+        result = self.select_sql(sql,data)
+        print(result)
         return result
 
 status_crud=CrudStatus()
