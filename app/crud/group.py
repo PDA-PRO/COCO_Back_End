766547751780cb0d2ad39da3bf71297ccae58f13
Jo MINSU <50093223+lookinmin@users.noreply.github.com>
2023-04-25 16:46:36 +0900

    <Feat> : Group
    1. 그룹 api에 리더 포함
    2. make group 하자마자 상세 넘어갈 수 있게 구현

--- app/crud/group.py
+++ app/crud/group.py
@@ -8,7 +8,7 @@ db_server = db.db_server
 class CrudGroup(Crudbase):
     def all_groups(self):
         sql = """
-            select g.id, g.name, g.desc, count(g.id) as members
+            select g.id, g.name, g.desc, g.leader, count(g.id) as members
             from coco.group as g, coco.group_users as u
             where g.id = u.group_id group by g.id;
         """
@@ -17,7 +17,7 @@ class CrudGroup(Crudbase):
     def mygroup(self, user_id):
         data = (user_id)
         sql = """
-            select g.id, g.name, g.desc, count(u.user_id) as members
+            select g.id, g.name, g.desc, g.leader, count(u.user_id) as members
             from coco.group as g, coco.group_users as u
             where u.group_id = g.id in (
                 select group_id from  coco.group_users where user_id = %s
@@ -39,6 +39,7 @@ class CrudGroup(Crudbase):
         for member in info.members:
             data = (last_idx, member)
             self.execute_sql(member_sql, data)
+        return last_idx
 
     def search_user(self, info):
         print(info)
