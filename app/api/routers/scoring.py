2f6b500d28bfe0fd8cca11381157ba946304da72
styughjvbn <3338761@naver.com>
2023-09-13 17:49:27 +0900

    <Feat> : wpc 확장 기능 추가 ---------------------------------------- 1. wpc 확장 기능 추가가 가능하도록 관련 코드 작성

--- app/api/routers/scoring.py
+++ app/api/routers/scoring.py
@@ -1,11 +1,12 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException,status
 from core import security
 from schemas.submission import Submit, subDetail
 from core.celery_worker import process_sub
 import redis
 from crud.submission import submission_crud
+from crud.task import task_crud
 from api.deps import get_cursor,DBCursor
-
+from core.wpc import *
 router = APIRouter()
 
 @router.post("/submission/", tags=["submission"])
@@ -51,3 +52,39 @@ async def load_result(sub_id: int,token: dict = Depends(security.check_token),db
         return rows[0]
     else:
         return None
+
+@router.get("/wpc", tags=["submission"])
+def get_wpc(sub_id:int,task_id:int,db_cursor:DBCursor=Depends(get_cursor)):
+    if not is_active:
+        raise HTTPException(status.HTTP_510_NOT_EXTENDED,"wpc 확장 기능이 존재하지 않습니다.")
+    
+    prev_result=submission_crud.read(db_cursor,db="coco",table="wpc",sub_id=sub_id)
+    if prev_result:
+        if prev_result[0]["status"]==2: #TC 실패로 틀린 제출이 아님
+            return {"status":2}
+        elif prev_result[0]["status"]==3: #wpc가 불가능한 문제
+            return {"status":3}
+        elif prev_result[0]["status"]==4: #wpc의 제한사항(512토큰 이하)을 초과
+            return {"status":4}
+        else:
+            return {"status":1,"wpc_result":prev_result[0]["result"]}
+    else:
+        task_title=task_crud.read(db_cursor,["title"],id=task_id)
+        print(task_title)
+        if "wpc:" not in task_title[0]["title"]:
+            submission_crud.create(db_cursor,{"sub_id":sub_id,"status":3},"coco","wpc")
+            return {"status":3}
+
+        sub_data=submission_crud.read(db_cursor,["code","message"],id=sub_id)
+        if sub_data[0]["message"]!="TC 실패":
+            submission_crud.create(db_cursor,{"sub_id":sub_id,"status":2},"coco","wpc")
+            return {"status":2}
+        
+        wpc_desc_id=task_title[0]["title"].split("wpc:")[-1]
+        wpc_result=process_wpc(sub_data[0]["code"],wpc_desc_id)
+        if wpc_result is None:
+            submission_crud.create(db_cursor,{"sub_id":sub_id,"status":4},"coco","wpc")
+            return {"status":4}
+        
+        submission_crud.create(db_cursor,{"sub_id":sub_id,"status":1,"result":wpc_result},"coco","wpc")
+        return {"status":1,"wpc_result":wpc_result}
---
+++ app/core/wpc.py
@@ -0,0 +1,13 @@
+import os
+
+is_active=False
+if os.path.exists("/".join(__file__.split("/")[:-1])+"/COCO_AI/"):
+    from core.COCO_AI.reference.wpc import WPC
+    is_active=True
+
+def process_wpc(code:str,wpc_desc_id:str):
+    if is_active:
+        wpc=WPC()
+        return wpc.process(code,wpc_desc_id)
+    else:
+        print("모듈 없음")
