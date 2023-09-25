28e6a6828a6412e12bb190d52cefefd66d9b43d2
styughjvbn <3338761@naver.com>
2023-09-26 00:36:40 +0900

    <feat> : room 인증 적용 및 일부 수정 ---------------------------------------- 1. room endpoint에서 인증이 필요한 것들에 jwt 토큰 확인 적용 CO-152 2. room schema 명칭 통일 3. 공통 응답모델 적용 4. request-tutor endpoint를 miscellaneous 라우터로 이동 5. jwt 토큰에 자신이 속하는 스터디룸을 모두 넣을 수 없기에 스터디룸 튜티 권한확인은 db연결하여 판단하는 함수 작성 6. 스터디룸 생성시 관련 테이블 생성 sql문 수정 7. 자잘한 오류 수정

--- app/core/security.py
+++ app/core/security.py
@@ -4,6 +4,8 @@ from passlib.context import CryptContext
 import os
 from fastapi import Depends,HTTPException,status
 from fastapi.security import OAuth2PasswordBearer
+from app.crud.room import room_crud
+from app.db.base import DBCursor
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -52,24 +54,27 @@ def get_password_hash(password: str) -> str:
 
 def decode_jwt(token: str) -> str:
     """
-    jwt를 검증하고 jwt의 role과 sub를 디코딩
+    jwt를 검증하고 jwt의 role과 sub, tutor를 디코딩
 
     params
     - token : jwt
     ---------------
     returns
-    - role : 유효한 jwt라면 role 리턴
-    - id : 유효한 jwt라면 id 리턴
+    - (role, id, tutor)
+        - role : 유효한 jwt라면 role 리턴
+        - id : 유효한 jwt라면 id 리턴
+        - tutor : 유요한 jwt라면 tutor 권한 리턴
     """
     try:
         payload = jwt.decode(token, os.getenv("SECRET_KEY"), algorithms=[os.getenv("ALGORITHM")])
         role: str = payload.get("role")#잘못된 토큰이면 에러발생
         id: str = payload.get("sub")#잘못된 토큰이면 에러발생
-        if role is None or id is None:
-            return "",""
+        tutor: str = payload.get("tutor")#잘못된 토큰이면 에러발생
+        if role is None or id is None or tutor is None:
+            return "","",""
     except JWTError:
-        return "",""
-    return role,id
+        return "","",""
+    return role,id,tutor
 
 def check_token(token: str = Depends(oauth2_scheme)):
     """
@@ -81,7 +86,7 @@ def check_token(token: str = Depends(oauth2_scheme)):
     - token : jwt
     ---------------
     returns
-    - {"role", "id"}
+    - {"role", "id", "tutor" }
     ---------------
     Raises:
     - credentials_exception: If token is invalid.
@@ -91,14 +96,38 @@ def check_token(token: str = Depends(oauth2_scheme)):
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
-    role,id=decode_jwt(token)
-    if role=="" or id=="":
+    role,id,tutor=decode_jwt(token)
+    if role=="" or id=="" or tutor=="":
         raise credentials_exception
-    return {"role":role,"id":id}
+    return {"role":role,"id":id,"tutor":tutor}
 
 def check_admin(token):
     if token['role']!=1:
-            raise HTTPException(
+        raise HTTPException(
+            status_code=403,
+            detail="권한이 없습니다.",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    
+def check_tutor(token):
+    if token['tutor']!=1:
+        raise HTTPException(
+            status_code=403,
+            detail="권한이 없습니다.",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    
+def check_leader(db_cursor: DBCursor, room_id:int,leader:str):
+    if not room_crud.read(db_cursor,['id'],leader=leader,id=room_id):
+        raise HTTPException(
+            status_code=403,
+            detail="권한이 없습니다.",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    
+def check_tutee(db_cursor: DBCursor, room_id:int,member:str):
+    if not room_crud.read(db_cursor,table='room_ids',user_id=member,room_id=room_id):
+        raise HTTPException(
             status_code=403,
             detail="권한이 없습니다.",
             headers={"WWW-Authenticate": "Bearer"},
