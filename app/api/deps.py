3236f213e6af2d134a6ea376b5aba654c9862571
styughjvbn <3338761@naver.com>
2023-10-05 00:28:59 +0900

    <build> : db 연결 수정 ---------------------------------------- 1. db 연결 수정

--- app/api/deps.py
+++ app/api/deps.py
@@ -1,8 +1,6 @@
 import pymysql
 from app import db
 from app.db.base import DBCursor
-
-
 import os
 from pymysql import converters
 
@@ -17,23 +15,19 @@ def get_cursor():
     db 연결 및 db cursor 획득
     정상 종료시 db 변경사항 commit 및 db 연결 종료
     """
-    con = pymysql.connect(host=db_server.host, user=db_server.user, password=db_server.password,port=db_server.port,
-                        db=db_server.db, charset='utf8')  # 한글처리 (charset = 'utf8')
-    # con = pymysql.connect(host=os.getenv("DATABASE_HOST"),
-    #                              port=3306,
-    #                              user=os.environ.get("DATABASE_USERNAME"),
-    #                              password=os.environ.get("DATABASE_PASSWORD"),
-    #                              database=os.environ.get("DATABASE"),
-    #                              cursorclass=pymysql.cursors.DictCursor,
-    #                              conv=converions)
+    # con = pymysql.connect(host=db_server.host, user=db_server.user, password=db_server.password,port=db_server.port, charset='utf8')  # 한글처리 (charset = 'utf8')
+    con = pymysql.connect(host=os.getenv("DATABASE_HOST"),
+                                 port=int(os.getenv("DATABASE_SOCKET")),
+                                 user=os.getenv("DATABASE_USERNAME"),
+                                 password=os.getenv("DATABASE_PASSWORD"),
+                                 database='coco',
+                                 charset='utf8',
+                                 conv=converions)
     cur = con.cursor(pymysql.cursors.DictCursor)
     try:
-        print("db 연결 생성")
         yield DBCursor(cursor=cur)
-        print("db 변경사항 적용")
         con.commit()
     # except :
     #     print("sql 오류로 연결을 종료합니다")
     finally:
-        print("db 연결 종료")
         con.close()
